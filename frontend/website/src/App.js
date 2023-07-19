75e59be00e7c179ad5ced2237f87e32c8975027b
venusgalstar <venusgalstar@gmail.com>
2023-07-19 16:36:26 +0900

    update frontend

--- frontend/website/src/App.js
+++ frontend/website/src/App.js
@@ -6,6 +6,7 @@ import Status from './components/Status';
 import Intro from './components/Intro';
 import Features from './components/Features';
 import Brand from './components/Brand';
+import Fotter from './components/Footer';
 
 function App() {
   return (
@@ -29,33 +30,7 @@ function App() {
       <Brand></Brand>
       <p className="color-coin-derivatives-5A9">Color Coin Derivatives</p>
       <p className="hawaii-whitelist-wCM">Hawaii Whitelist</p>
-      <div className="auto-group-dcsw-Ex9">
-        <div className="frame-8-Mms">
-          <div className="ellipse-2-hqj">
-          </div>
-          <p className="governance-F6Z">Governance</p>
-        </div>
-        <div className="frame-9-PCm">
-          <div className="ellipse-4-gxZ">
-          </div>
-          <p className="platform-fees-RQM">Platform Fees</p>
-        </div>
-        <div className="frame-10-RYm">
-          <div className="ellipse-3-NU1">
-          </div>
-          <p className="digital-nfts-Wq7">Digital NFTs</p>
-        </div>
-        <div className="frame-11-Sim">
-          <div className="ellipse-5-o3X">
-          </div>
-          <p className="irl-token-gates-6YR">IRL Token Gates</p>
-        </div>
-        <div className="frame-12-ScH">
-          <div className="ellipse-6-ng9">
-          </div>
-          <p className="physical-asset-management-jLV">Physical Asset Management</p>
-        </div>
-      </div>
+      <Footer></Footer>
       <img className="x1000-1-Rz1" src="./assets/x1000-1.png" />
     </div>
   );
---
+++ frontend/website/src/components/Footer.js
@@ -0,0 +1,38 @@
+
+
+function Brand(props) {
+
+    return (
+
+        <div className="auto-group-dcsw-Ex9">
+            <div className="frame-8-Mms">
+                <div className="ellipse-2-hqj">
+                </div>
+                <p className="governance-F6Z">Governance</p>
+            </div>
+            <div className="frame-9-PCm">
+                <div className="ellipse-4-gxZ">
+                </div>
+                <p className="platform-fees-RQM">Platform Fees</p>
+            </div>
+            <div className="frame-10-RYm">
+                <div className="ellipse-3-NU1">
+                </div>
+                <p className="digital-nfts-Wq7">Digital NFTs</p>
+            </div>
+            <div className="frame-11-Sim">
+                <div className="ellipse-5-o3X">
+                </div>
+                <p className="irl-token-gates-6YR">IRL Token Gates</p>
+            </div>
+            <div className="frame-12-ScH">
+                <div className="ellipse-6-ng9">
+                </div>
+                <p className="physical-asset-management-jLV">Physical Asset Management</p>
+            </div>
+        </div>
+    )
+}
+
+export default Brand;
+
